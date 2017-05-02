0619ee827b7026a9aab037a9222c08e05ca6a39a
Raúl Elías <rauleliasmezamora@gmail.com>
2017-05-02 12:04:42 -0300

    Fix server hour and template custom email

--- resources/views/emails/users/sign_up.blade.php
+++ resources/views/emails/users/sign_up.blade.php
@@ -1,30 +1,26 @@
 @component('mail::message')
 # Bienvenido(a) {{ $user->name }}
-
-Ya tienes acceso a nuestra plataforma de <b>Enexpro</b>
-
-Tus credenciales de acceso son las siguientes:
+Ya tienes acceso a nuestra plataforma de <b>Enexpro</b>. Tus credenciales de acceso son las siguientes:
 <br />
-
-    Usuario: {{ $user->email }}
-    Contraseña: {{ $password }}
-    Rol: {{ $user->role->name }}
-
 <br />
-Te sugerimos que cambies la contraseña y tu imagen de perfil inmediatamente desde aquí
-
-@component('mail::button', ['url' =>  getenv('APP_URL') . '/login'])
-    Ingresar
+@component('mail::panel')
+Usuario: {{ $user->email }}
+<br />
+Contraseña: {{ $password }}
 @endcomponent
 <br />
+<br />
+Te sugerimos que cambies la contraseña y tu imagen de perfil inmediatamente desde aquí
+@component('mail::button', ['url' =>  getenv('APP_URL') . '/login'])
+Editar Perfil
+@endcomponent
 <br />
 Saludos.
 <br>
 Equipo {{ config('app.name') }}.
-
 @component('mail::subcopy')
-    Si tiene problemas al hacer click en el botón, copie y pegue el siguiente enlace en su navegador
-    <a url="{{ '/login' }}" target="_blank">{{ getenv('APP_URL') . '/login' }}</a>
+Si tiene problemas al hacer click en el botón, copie y pegue el siguiente enlace en su navegador
+<a url="{{ '/login' }}" target="_blank">{{ getenv('APP_URL') . '/login' }}</a>
 @endcomponent
 
 @endcomponent
