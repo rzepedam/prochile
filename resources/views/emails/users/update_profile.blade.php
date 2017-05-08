9d92b3d86a045b0c84bef67b2bf7393bb832df28
Raúl Elías <rauleliasmezamora@gmail.com>
2017-05-08 17:01:48 -0300

    Fix bug in email update

--- resources/views/emails/users/update_profile.blade.php
+++ resources/views/emails/users/update_profile.blade.php
@@ -1,7 +1,6 @@
 @component('mail::message')
 # {{ $user->name }}
 Tu perfil ha sido actualizado satisfactoriamente.
-<img src="{{ $message->embed(assets('/img/gender.png')) }}">
 <br />
 <br />
 Saludos.
