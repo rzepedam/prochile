35372a685fc25b2ef7090235953bfd73c6db60bc
Raúl Elías <rauleliasmezamora@gmail.com>
2017-05-12 07:49:34 -0300

    xxx

--- resources/views/emails/users/8am.blade.php
+++ resources/views/emails/users/8am.blade.php
@@ -1,11 +1,12 @@
 @component('mail::message')
 # Buenos días,
 <br />
-Le recordamos que la acreditación a **ENEXPRO**, se realizará en **ala Sur, del 2° piso Hotel Antofagasta a través de la empresa Control QTIME**.
+Le recordamos que la acreditación a **Enexpro**, se realizará en ala Sur, del 2° piso Hotel Antofagasta a través de la empresa **Controlqtime**.
 <br />
 <br />
+***
 <br />
-Prochile informs that  the accreditation to **ENEXPRO**, will be held in the South wing of the 2nd floor at Hotel Antofagasta through Control Qtime company.
+Prochile informs that  the accreditation to **Enexpro**, will be held in the South wing of the 2nd floor at Hotel Antofagasta through **Controlqtime** company.
 <br />
 <br />
 <br />
