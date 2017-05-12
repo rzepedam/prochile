444732d021a0dde3385fdef0def9e9b9f2e2f4d1
Raúl Elías <rauleliasmezamora@gmail.com>
2017-05-12 00:10:47 -0300

    xxx

--- resources/views/emails/users/8am.blade.php
+++ resources/views/emails/users/8am.blade.php
@@ -1,6 +1,8 @@
 @component('mail::message')
 # Buenos días,
+<br />
 Le recordamos que la acreditación a **ENEXPRO**, se realizará en **ala Sur, del 2° piso Hotel Antofagasta a través de la empresa Control QTIME**.
+<br />
 <br />
 <br />
 Saludos.
