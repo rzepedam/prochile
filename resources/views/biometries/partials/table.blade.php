8bae7f3e9e5c69a2961d1363e1ff5d995c270406
Raúl Elías <rauleliasmezamora@gmail.com>
2017-05-12 00:21:23 -0300

    Fix index table Biometry

--- resources/views/biometries/partials/table.blade.php
+++ resources/views/biometries/partials/table.blade.php
@@ -8,13 +8,15 @@
             </tr>
         </thead>
         <tbody>
+            <?php $i = 0; ?>
             @foreach($biometries as $biometry)
                 @if ($biometry->is_active === 'True')
                     <tr>
-                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $i + 1 }}</td>
                         <td>{{ $biometry->first_name . ' ' . $biometry->last_name }}</td>
                         <td class="text-center">{{ \ProChile\Http\Helpers\Helper::rut($biometry->rut) }}</td>
                     </tr>
+                    <?php $i ++; ?>
                 @endif
             @endforeach
         </tbody>
